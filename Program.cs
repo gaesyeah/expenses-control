259b09e5e56736a5b17058b24aa8db18fd3c59c3
gaesyeah <gabrielgoesrios@gmail.com>
2026-07-16 18:41:39 -0300

    chore: add Dockerfile and enable Scalar docs outside Development

--- Program.cs
+++ Program.cs
@@ -5,8 +5,6 @@ using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddScoped<ExpensesControlContext>();
 builder.Services.AddExceptionHandler<ValidationExceptionHandler>();
@@ -14,13 +12,9 @@ builder.Services.AddProblemDetails();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-    app.MapScalarApiReference();
-}
-else
+app.MapOpenApi();
+app.MapScalarApiReference();
+if (!app.Environment.IsDevelopment())
 {
     app.UseHttpsRedirection();
 }
