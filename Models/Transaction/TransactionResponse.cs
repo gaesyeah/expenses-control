4e2c124fdc53bff838f649b2c624d10981b8122e
gaesyeah <gabrielgoesrios@gmail.com>
2026-07-19 14:43:31 -0300

    feat: add person response in transaction response

--- Models/Transaction/TransactionResponse.cs
+++ Models/Transaction/TransactionResponse.cs
@@ -1,7 +1,7 @@
 namespace ExpensesControl.Models;
 
-public record TransactionResponse(Guid Id, string Description, decimal Value, TransactionType Type, Guid PersonId, string PersonName)
+public record TransactionResponse(Guid Id, string Description, decimal Value, TransactionType Type, PersonResponse Person)
 {
   public TransactionResponse(TransactionModel transaction)
-    : this(transaction.Id, transaction.Description, transaction.Value, transaction.Type, transaction.PersonId, transaction.Person.Name) { }
+    : this(transaction.Id, transaction.Description, transaction.Value, transaction.Type, new PersonResponse(transaction.Person)) { }
 }
